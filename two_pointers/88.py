167ad37e43b3ce5e8d95e3277ca3a44ba0f2b892
Anonymous <tranhuuquanghuyh74@gmail.com>
2026-03-17 10:05:19 +0700

    88. Merge Sorted Array

---
+++ two_pointers/88.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+       
+        write_index = m + n - 1
+
+        nums1_index = m - 1
+        nums2_index = n - 1
+
+        while nums2_index >= 0:
+            if nums1_index >= 0 and nums1[nums1_index] > nums2[nums2_index]:
+                nums1[write_index] = nums1[nums1_index]
+                nums1_index -= 1
+            else:
+                nums1[write_index] = nums2[nums2_index]
+                nums2_index -= 1
+
+            write_index -= 1
